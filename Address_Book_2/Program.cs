caf2225daf3d8f49d7a2b9ad53879d91e00ec8ba
mabbuprem <mabbupremkumar@gmail.com>
2022-04-07 13:38:57 +0530

    [prem] FEATURE ADDED: Ability to add more than one person contacts i…

--- Address_Book_2/Program.cs
+++ Address_Book_2/Program.cs
@@ -10,8 +10,16 @@ namespace Address_Book_2
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            Contacts C1 = new Contacts();
-            C1.AddContacts();
+            string chooseOptionToAddContacts = string.Empty;
+            List<Contacts> book = new List<Contacts>();
+            do
+            {
+                Contacts C1 = new Contacts();
+                C1.AddContacts();
+                book.Add(C1);
+                Console.WriteLine("Enter the choice to add more persons Contacts in current Address Book: \n \"Yes\" or \"No\"");
+                chooseOptionToAddContacts = Console.ReadLine().ToUpper();
+            } while (chooseOptionToAddContacts.Contains("Y"));
         }
     }
 }
